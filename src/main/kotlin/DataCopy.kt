736d763264dc6baf42caa7114ce8814ce72bd39a
Alejandro Serrano Mena <alejandro.serrano.mena@jetbrains.com>
2024-11-22 15:57:50 +0100

    Do not use reflect.jvm

--- src/main/kotlin/DataCopy.kt
+++ src/main/kotlin/DataCopy.kt
@@ -4,10 +4,7 @@ import java.lang.invoke.MethodHandle
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.MethodType
 import kotlin.reflect.KFunction
-import kotlin.reflect.KProperty1
 import kotlin.reflect.javaType
-import kotlin.reflect.jvm.javaGetter
-import kotlin.reflect.jvm.javaMethod
 
 object DataCopy {
     @OptIn(ExperimentalStdlibApi::class)
@@ -24,20 +21,19 @@ object DataCopy {
         val kotlinKlass = klass.kotlin
         require(kotlinKlass.isData) { "Only data classes are supported" }
         require(name == "copy") { "Only copy is supported" }
-        val copyInfo = kotlinKlass.members.singleOrNull() { it.name == "copy" }
-        requireNotNull(copyInfo as? KFunction<*>) { "No copy method found" }
+        val copyHandle = klass.declaredMethods.singleOrNull { it.name == "copy" }
+        val copyInfo = kotlinKlass.members.singleOrNull { it.name == "copy" }
+        require(copyHandle != null && copyInfo is KFunction<*>) { "No copy method found" }
 
         // create list of arguments
         val getters = mutableListOf<MethodHandle>()
         val indices = mutableListOf<Int>(0) // always start with 'this'
         for (parameter in copyInfo.parameters.drop(1)) {
-            val parameterName = parameter.name
+            val parameterName = requireNotNull(parameter.name)
             when (val index = givenProperties.indexOf(parameterName)) {
                 -1 -> {
-                    val property = requireNotNull(kotlinKlass.members.singleOrNull { it.name == parameterName }) {
-                        "No property '$parameterName' found"
-                    }
-                    val getter = requireNotNull((property as? KProperty1<*, *>)?.javaGetter) {
+                    val getterName = "get${parameterName.replaceFirstChar { it.uppercaseChar() }}"
+                    val getter = requireNotNull(klass.getDeclaredMethod(getterName)) {
                         "No getter for '$parameterName' found"
                     }
                     getters += lookup.unreflect(getter)
@@ -50,7 +46,7 @@ object DataCopy {
             }
         }
 
-        val copyWithGetters = MethodHandles.filterArguments(lookup.unreflect(copyInfo.javaMethod), 1, *getters.toTypedArray())
+        val copyWithGetters = MethodHandles.filterArguments(lookup.unreflect(copyHandle), 1, *getters.toTypedArray())
         val copyReordered = MethodHandles.permuteArguments(copyWithGetters, type, *indices.toIntArray())
         return ConstantCallSite(copyReordered)
     }
